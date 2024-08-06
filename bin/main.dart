e38ea1c6e46d68926559efdbc57a632389a3206a
ricarthlima <rrsl@cin.ufpe.br>
2024-08-06 14:24:24 -0300

    v3.1 - pilha de execucao

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,27 @@
 import 'package:dart_exceptions/screens/account_screen.dart';
 
+// void main() {
+//   AccountScreen accountScreen = AccountScreen();
+//   accountScreen.initializeStream();
+//   accountScreen.runChatBot();
+// }
+
 void main() {
-  AccountScreen accountScreen = AccountScreen();
-  accountScreen.initializeStream();
-  accountScreen.runChatBot();
+  print("Começou a main");
+  function01();
+  print("Finalizou a main");
+}
+
+void function01() {
+  print("Começou a Função 01");
+  function02();
+  print("Finalizou a Função 01");
+}
+
+void function02() {
+  print("Começou a Função 02");
+  for (int i = 1; i <= 5; i++) {
+    print(i);
+  }
+  print("Finalizou a Função 02");
 }
