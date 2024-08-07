7fb155ca93d8d0767db5d8488396bbc153fdf239
ricarthlima <rrsl@cin.ufpe.br>
2024-08-07 14:38:40 -0300

    v4.2 - capturando contas para transação

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,15 @@
 import 'package:dart_exceptions/screens/account_screen.dart';
+import 'package:dart_exceptions/services/transaction_service.dart';
 
 void main() {
-  AccountScreen accountScreen = AccountScreen();
-  accountScreen.initializeStream();
-  accountScreen.runChatBot();
+  TransactionService().makeTransaction(
+    idSender: "ID001",
+    idReceiver: "ID002",
+    amount: 5,
+  );
+  // AccountScreen accountScreen = AccountScreen();
+  // accountScreen.initializeStream();
+  // accountScreen.runChatBot();
 }
 
 // void main() {
---
+++ lib/services/transaction_service.dart
@@ -0,0 +1,25 @@
+import 'package:dart_exceptions/models/account.dart';
+import 'package:dart_exceptions/services/account_service.dart';
+
+class TransactionService {
+  final AccountService _accountService = AccountService();
+
+  makeTransaction({
+    required String idSender,
+    required String idReceiver,
+    required double amount,
+  }) async {
+    List<Account> listAccount = await _accountService.getAll();
+
+    Account senderAccount = listAccount.firstWhere(
+      (acc) => acc.id == idSender,
+    );
+
+    Account receiverAccount = listAccount.firstWhere(
+      (acc) => acc.id == idReceiver,
+    );
+
+    print(senderAccount);
+    print(receiverAccount);
+  }
+}
